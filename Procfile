206d90c6c0a09617957a95068e6de615f5292435
Aya Shigure <ayatk@users.noreply.github.com>
2015-12-06 18:34:44 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: bundle exec ruby update_name.rb
---
+++ update_name.rb
@@ -0,0 +1,56 @@
+# coding: utf-8
+require 'twitter'
+
+puts "[System] stert update_name server"
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+end
+
+stream_client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+end
+sn = 'AyaTokikaze'
+
+stream_client.user do |status|
+  # 自分以外の人がupdate_nameできるかどうか
+  # default: false
+  permission = false
+  next unless status.is_a? Twitter::Tweet
+  next if status.text.start_with? "RT"
+  when condition
+
+  if status.text =~ /^(?!RT)(.*@#{sn}\supdate_name\s((.|\n)+?)|.+?\(@#{sn}\))$/
+    # 許可されていないユーザーのupdate_nameをブロック
+    if status.user.screen_name != sn && !permission
+      option = {"in_reply_to_status_id" => status.id.to_s}
+      tweet = "@#{status.user.screen_name} その操作は許可されていません"
+      client.update tweet,option
+      puts "[System] Blocked rename -> \'#{name}\' by @#{status.user.screen_name}"
+      next
+    end
+
+    name = status.text.gsub("@#{sn}\supdate_name\s","")
+    name = status.text.gsub("(@#{sn})","")
+    name.strip!
+
+    if name.length > 20 # 変更する名前が20文字以上の時
+      option = {"in_reply_to_status_id" => status.id.to_s}
+      tweet = "@#{status.user.screen_name} 「#{name}」は文字数オーバーです"
+      client.update tweet,option
+      puts "[System] over naming -> \'#{name}\' by @#{status.user.screen_name}"
+    else
+      client.update_profile(:name => name)
+      option = {"in_reply_to_status_id" => status.id.to_s}
+      tweet = "@#{status.user.screen_name} 「#{name}」に名前を変えました"
+      client.update tweet,option
+      puts "[System] Renamed -> \'#{name}\' by @#{status.user.screen_name}"
+    end
+  end
+end
